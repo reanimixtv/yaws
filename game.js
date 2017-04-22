1c3776c6a233e76b8282eaf1f2546bd8a0f7331f
Kevin Epps <kepps@cisco.com>
2017-04-22 16:25:43 -0400

    intial push of all code

---
+++ game.js
@@ -0,0 +1,28 @@
+
+BasicGame.Game = function (game) {
+
+};
+
+BasicGame.Game.prototype = {
+
+  create: function () {
+
+    this.sea = this.add.tileSprite(0, 0, 1024, 768, 'sand');
+
+  },
+
+  update: function () {
+    this.sea.tilePosition.y += 0.7;
+  },
+
+  quitGame: function (pointer) {
+
+    //  Here you should destroy anything you no longer need.
+    //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+
+    //  Then let's go back to the main menu.
+    this.state.start('MainMenu');
+
+  }
+
+};
